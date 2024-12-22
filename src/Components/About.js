84a4fdda0441bf94edead3f6adeb4e6616632368
mahedy25 <mahedyhasan873@gmail.com>
2024-12-22 14:41:36 +0600

    none

--- src/Components/About.js
+++ src/Components/About.js
@@ -58,7 +58,7 @@ const About = () => {
         <div className='col-span-3 rounded-lg w-full justify-end items-start flex'>
           {/* image */}
           <Image
-          src='/images/profile2.jpg'
+          src="https://via.placeholder.com/400"
           width={400}
           height={400}
           alt='Profile Picture'
